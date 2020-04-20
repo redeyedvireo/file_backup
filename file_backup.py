baef381f950332cfbf6299b6f1bf43c3f4caa99f
jg572h <jg572h@att.com>
2020-04-20 11:13:53 -0400

    First pass at encryption.

--- file_backup.py
+++ file_backup.py
@@ -1,22 +1,42 @@
 from zipfile import ZipFile, ZIP_DEFLATED
+from cryptography.fernet import Fernet
 import os.path
+import sys
+
+zipFilePath = 'c:\\temp\\file-backup.zip'
+
+def encryptFile(fileName, key):
+    with open(fileName) as fp:
+        contents = fp.read()
+        print('Original:')
+        print(contents)
+
+        tempKey = Fernet.generate_key()
+        encrKey = Fernet(tempKey)
+        messageBytes = bytes(contents, 'utf8')
+        encryptedContents = encrKey.encrypt(messageBytes)
+
+        with open('test_encrypted.bin', 'wb') as outputFile:
+            outputFile.write(encryptedContents)
 
 
-def readFileList():
-    with open('filelist.cfg') as fp:
+def readFileList(fileName):
+    with open(fileName) as fp:
         lines = fp.readlines()
         fileList = [line.strip() for line in lines]
         return fileList
 
-    # In case of error
-    return []
+
+def createZipFile(filePath):
+    # Note that if no compression parameter is given, the resulting zip file will not be compressed.
+    with ZipFile(filePath, mode='w', compression=ZIP_DEFLATED) as myzip:
+        for file in fileList:
+            fileBaseName = os.path.basename(file)
+            myzip.write(file, arcname=fileBaseName)
 
 
-fileList = readFileList()
+# ------------------ Start ------------------
+encryptFile('filelist.cfg', 'mykey')
 
-# Note that if no compression parameter is given, the resulting zip file will not be compressed.
-with ZipFile('c:\\temp\\file-backup.zip', mode='w', compression=ZIP_DEFLATED) as myzip:
-    for file in fileList:
-        fileBaseName = os.path.basename(file)
-        myzip.write(file, arcname=fileBaseName)
-
+#fileList = readFileList('filelist.cfg')
+#createZipFile(zipFilePath)
